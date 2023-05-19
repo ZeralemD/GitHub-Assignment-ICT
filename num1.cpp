f7f04a255c28b7d55a1d7227541456dc91fbe426
ZeralemD <123317441+ZeralemD@users.noreply.github.com>
2023-05-19 05:25:50 +0300

    Create num1.cpp
    Group members: Zeralem Dires, Sarah Yilma, and Yanet Daniel.

---
+++ num1.cpp
@@ -0,0 +1,42 @@
+<html>
+<title> ICT Project </title>
+<head>
+<style>
+#by {
+font-family: Anaheim;
+background-color: lightgrey;
+margin: 20px;
+font-size: 1.2em;
+border: 2px solid white;
+padding: 20px;
+}
+h3 {
+background-color: #FAF9F6;
+margin-right: 65rem;
+}
+.za {
+background-color: #FAF9F6;
+margin-right: 63rem;
+}
+.solid {
+border-top: 3px solid white;
+}
+</style>
+</head>
+<body id="by">
+<u> <h2>Andinet International School Survey Form </h2> </u>
+<p> Thank you for taking your time to fill out this form. </p> 
+ <h3> Personal Detail </h3> 
+<form>
+ <label for="fname" >First name:</label>
+ <input type="text" id="fname" name="fname" placeholder="Enter name here" 
+required="required">
+<br><br>
+ <label for="lname">Last name:</label>
+ <input type="text" id="lname" name="lname" placeholder="Enter name here" 
+required>
+<br><br>
+<label for="email"> Email: </label>
+ <input type="email" id="email" name="email" placeholder="Enter email here" 
+required>
+<br><br>
